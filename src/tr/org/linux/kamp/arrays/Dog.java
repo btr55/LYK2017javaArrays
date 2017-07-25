a5bf7b0190da1e51e3123ca29e9b63580fcc32b9
Fatih Erdal <fatiherdal0655@gmail.com>
2017-07-25 19:11:05 +0300

    ilk atış

---
+++ src/tr/org/linux/kamp/arrays/Dog.java
@@ -0,0 +1,18 @@
+package tr.org.linux.kamp.arrays;
+
+public class Dog {
+	
+	String name;
+	
+	public static void main(String[] args) {
+		
+		String[]myStringArray= new String[3];
+		String[]myAnotherStringArray=null;
+		String[]myStringArray2= {"Yavuz","Kerim","Ahmet"};
+		
+		
+		System.out.println(myStringArray2[1]);
+		
+	}
+
+}
